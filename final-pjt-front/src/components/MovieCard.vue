5d08ac1c6e970f9b62e6149e5d3d717cfacfa311
unknown <sea962@gmail.com>
2021-05-20 16:59:51 +0900

    1차 check

---
+++ final-pjt-front/src/components/MovieCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img :src="srcUrl" alt="poster_path">
+    <p>{{ movie.title }}</p>
+    <p>{{ movie.overview }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieCard',
+  props: {
+    movie: {
+      type: Object
+    },
+  },
+  computed: {
+    srcUrl: function () {
+      console.log(this.movie.poster_path)
+      return 'https://image.tmdb.org/t/p/w500' + this.movie.poster_path
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- final-pjt-front/src/views/Movie.vue
+++ final-pjt-front/src/views/Movie.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
-    <h1>Movie Page</h1>
-    
-    <p>{{movies}}</p>
-  </div>
+<div>
+  <h1>Movie Page</h1>
+  <section>
+    <b-card-group row row-cols-1 row-cols-md-2 g-4>
+      <MovieCard
+      v-for="(movie, idx) in movies"
+      :key="idx"
+      :movie="movie"
+      />
+    </b-card-group>
+  </section>
+</div>
 </template>
 
 <script>
+import MovieCard from '@/components/MovieCard.vue'
 import { mapActions, mapState } from 'vuex'
 
 export default {
   name: 'Movie',
+  components: {
+    // 2. 등록하기
+    MovieCard
+  },
   methods: {
     ...mapActions(['getMoviesFromServer']),
   },
