23ee66acb28752aa3dbd66f97b75351df6466237
soc4585 <soc4585@gmail.com>
2021-05-26 16:31:09 +0900

    movie comment 수정 개선

--- final-pjt-front/src/components/movies/MovieCommentUpdateForm.vue
+++ final-pjt-front/src/components/movies/MovieCommentUpdateForm.vue
@@ -5,7 +5,7 @@
         <div class="modal-content">
 
           <div class="modal-header">
-            <h5 class="modal-title" :id="'exampleModalLabel'+comment.id">수정</h5>
+            <h5 class="modal-title" :id="'exampleModalLabel'+comment.id">평점 수정</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
 
