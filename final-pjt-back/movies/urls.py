3d698dbaa082ffc911e9384217f2999410d96920
soc4585 <soc4585@gmail.com>
2021-05-28 00:24:35 +0900

    url 주석

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -3,12 +3,20 @@ from . import views
 
 
 urlpatterns = [
+    # get
     path('', views.movie_list),
+    # get
     path('<int:movie_id>/', views.movie),
-    path('<int:movie_id>/likes/', views.like),
+    # get post
     path('<int:movie_id>/comment/', views.comment_list_create),
+    # put delete
     path('<int:movie_id>/comment/<int:comment_id>/', views.update_delete_comment),
+    # get post
+    path('<int:movie_id>/likes/', views.like),
+    # get
     path('recommend/', views.recommend_movie),
+    # get
     path('liked/', views.liked_movies),
+    # get
     path('commented/', views.commented_movies),
 ]
