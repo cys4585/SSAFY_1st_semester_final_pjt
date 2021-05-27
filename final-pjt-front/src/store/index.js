fd29e5f33bfa1a1ce8026468db06164a0fa3bcc2
soc4585 <soc4585@gmail.com>
2021-05-27 21:13:07 +0900

    ...

--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -229,7 +229,7 @@ export default new Vuex.Store({
         })
     },
     getMoviesFromServer: function ({ commit }, pageNum) {
-      console.log(pageNum)
+      // console.log(pageNum)
       // console.log('getMoviesFromServer() 실행')
       axios({
         method: 'get',
